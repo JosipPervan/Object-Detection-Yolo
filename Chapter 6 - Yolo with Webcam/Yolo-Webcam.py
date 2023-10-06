40046e508ea78284615ce3480d03c9e598ef60b4
Josip Pervan <josip.tomica01@gmail.com>
2023-10-06 12:53:04 +0200

    Dodan tekst koji ispisuje detektirani objekat, nazivi "person" i "cell phone" promjenjeni na hrvatski jezik

--- Chapter 6 - Yolo with Webcam/Yolo-Webcam.py
+++ Chapter 6 - Yolo with Webcam/Yolo-Webcam.py
@@ -10,7 +10,17 @@ cap.set(4, 720)
 
 model = YOLO("../Yolo-Weights/yolov8n.pt")
 
-
+classNames = ["osoba", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "mobitel",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 
 while True:
@@ -20,6 +30,7 @@ while True:
         boxes = r.boxes
         for box in boxes:
 
+            #Box oko objekta
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 255), 3)
@@ -27,9 +38,15 @@ while True:
             w, h = x2-x1, y2-y1
             cvzone.cornerRect(img, (x1,y1,w,h))
 
+
+            #Confidence
             conf = math.ceil((box.conf[0]*100))/100
-            print(conf)
-            cvzone.putTextRect(img, f'{conf}', (max(0, x1), max(35, y1)))
+
+
+            #Class name
+            cls = int( box.cls[0])
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)))
+
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
